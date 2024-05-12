f5dc43d7d61d50a01fc75a386f3550a63bbb8698
muhammadrafi121 <rafi19547@gmail.com>
2024-05-12 11:02:07 +0700

    add solution for problem 1.04

--- main.exs
+++ main.exs
@@ -26,6 +26,12 @@ defmodule Main do
       _ -> at(k-1, rest)
     end
   end
+
+  @doc """
+    solution for problem 1.04
+  """
+  def list_length([]), do: 0
+  def list_length([_ | xs]), do: 1 + list_length(xs)
 end
 
 ExUnit.start()
@@ -62,5 +68,15 @@ defmodule MainTest do
     assert at(1, []) == nil
     assert at(2, [1]) == nil
     assert at(20, Enum.to_list(1..100)) == 20
+  end
+
+  @doc """
+    test for problem 1.04
+  """
+  test "number of elements of a list" do
+    assert list_length([]) == 0
+    assert list_length([1]) == 1
+    assert list_length([1, 2, 3]) == 3
+    assert list_length(Enum.to_list(1..1000)) == 1000
   end
 end
